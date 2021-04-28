4f7b8ca435e132a21dae824b5c73b84c2303c087
Christian Marquez <christianmarquez41@gmail.com>
2021-04-28 14:23:00 -0400

    added button with id of the guild mate

--- public/js/displayGuildmates.js
+++ public/js/displayGuildmates.js
@@ -1,16 +1,22 @@
 export default function displayGuildmates(guildMate) {
-        // Create table data
+    console.log(guildMate);
+        // Create table row with its elements
         const tableRow = document.createElement("tr")
+        tableRow.setAttribute("data-id", guildMate._id)
+
         const name = document.createElement("td")
         const startingGP = document.createElement("td")
         const twDefense = document.createElement("td")
         const twOffense = document.createElement("td")
+        const button = document.createElement("button")
+        button.setAttribute("data-id", guildMate._id)
         // Update table text
         name.textContent = guildMate.name
         startingGP.textContent = guildMate.startingGP
         twDefense.textContent = guildMate.twDefense
         twOffense.textContent = guildMate.twOffense
+        button.textContent = "Edit"
         // Append table tada to table
-        tableRow.append(name, startingGP, twDefense, twOffense)
+        tableRow.append(name, startingGP, twDefense, twOffense, button)
         table.append(tableRow)
 }
