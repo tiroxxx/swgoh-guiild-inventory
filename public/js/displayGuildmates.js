48309de18af40ec201d08234d2f2328706c36c11
Christian Marquez <christianmarquez41@gmail.com>
2021-04-28 13:33:27 -0400

    separated displaying guild mates from getting guild mates fro mdb

---
+++ public/js/displayGuildmates.js
@@ -0,0 +1,18 @@
+export default function displayGuildmates(guildMates) {
+    guildMates.forEach(guildMate => {
+        // Create table data
+        const tableRow = document.createElement("tr")
+        const name = document.createElement("td")
+        const startingGP = document.createElement("td")
+        const twDefense = document.createElement("td")
+        const twOffense = document.createElement("td")
+        // Update table text
+        name.textContent = guildMate.name
+        startingGP.textContent = guildMate.startingGP
+        twDefense.textContent = guildMate.twDefense
+        twOffense.textContent = guildMate.twOffense
+        // Append table tada to table
+        tableRow.append(name, startingGP, twDefense, twOffense)
+        table.append(tableRow)
+    })
+}
--- public/js/saveGuildmates.js
+++ public/js/saveGuildmates.js
@@ -2,8 +2,6 @@ export default async function saveGuildmates(guildMate) {
     console.log("saved mates");
     try {
         const response = await axios.post("http://localhost:3000/guildmates", guildMate)
-
-        
     }
     catch (err) {
         console.error(err)
