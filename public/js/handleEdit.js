656c8c6c5fb59249340fa38b746c216388885908
Christian Marquez <christianmarquez41@gmail.com>
2021-04-29 13:07:53 -0400

    created modal and added existing info as placeholder

--- public/js/handleEdit.js
+++ public/js/handleEdit.js
@@ -2,17 +2,24 @@ import handleEditSubmit from "./handleEditSubmit.js"
 
 export default function handleEdit() {
     const guildMateId = this.dataset.id
+    // Modal elements
     const modalForm = document.querySelector("#modal-form")
     const modal = document.querySelector("#modal");
     const modalName = document.querySelector("#modal-name")
-    const modalStartingGP = document.querySelector("modal-starting-gp")
-    const modalTwDefense = document.querySelector("modal-tw-defense")
-    const modalTwOffense = document.querySelector("modal-tw-offense")
+    const modalStartingGP = document.querySelector("#modal-starting-gp")
+    const modalTwDefense = document.querySelector("#modal-tw-defense")
+    const modalTwOffense = document.querySelector("#modal-tw-offense")
+    // Guild mates' info from table
     const guildMateInfo = document.querySelector(`[data-id="${guildMateId}"]`)
     const guildMateName = guildMateInfo.childNodes[0].textContent
     const guildMateStartingGP = guildMateInfo.childNodes[1].textContent
     const guildMateTwDefense = guildMateInfo.childNodes[2].textContent
     const guildMateTwOffense = guildMateInfo.childNodes[3].textContent
+    // Add exixting info as placeholder
+    modalName.setAttribute("value", guildMateName)
+    modalStartingGP.setAttribute("value", guildMateStartingGP)
+    modalTwDefense.setAttribute("value", guildMateTwDefense)
+    modalTwOffense.setAttribute("value", guildMateTwOffense)
 
     console.log(guildMateName, guildMateStartingGP, guildMateTwDefense, guildMateTwOffense);
     modal.style.display = "block"
