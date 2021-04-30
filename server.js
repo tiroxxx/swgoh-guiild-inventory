668bd62515295de6ba90039d2a9a02274d323f98
Christian Marquez <christianmarquez41@gmail.com>
2021-04-30 18:36:35 -0400

    deleted commented code from server

--- server.js
+++ server.js
@@ -14,12 +14,6 @@ db.once("open", () => console.log("connected to db"))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
-//     next();
-// });
 app.use(express.static("public"))
 
 const guildmatesRouter = require("./routes/Guildmates")
