596d12b834ccae6728cbd072a24d4428bb64013c
Christian Marquez <christianmarquez41@gmail.com>
2021-04-30 18:44:37 -0400

    trying to deploy

--- server.js
+++ server.js
@@ -5,12 +5,11 @@ const mongoose = require("mongoose")
 const axios = require("axios")
 require("dotenv").config()
 
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
 
-// mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-// const db = mongoose.connection
-
-// db.on("error", err => console.error(error))
-// db.once("open", () => console.log("connected to db"))
+db.on("error", err => console.error(error))
+db.once("open", () => console.log("connected to db"))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
